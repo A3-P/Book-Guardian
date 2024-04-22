eb0d7e3b4223055c41603f440ac22ae428869753
wendelFacul <161367279+wendellastfacul@users.noreply.github.com>
2024-04-22 19:16:57 -0300

    Add routes
    Signed-off-by: wendelFacul <161367279+wendellastfacul@users.noreply.github.com>

--- bookguardian/views.py
+++ bookguardian/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from . import models
 
-# Create your views here.
+
+class HomeList(ListView):
+    model = models.BookGuardian
+    template_name = "index.html"
+    context_object_name = "bookguardian"

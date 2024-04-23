9630dcc3157506864f19eee365a99f16c0d420da
wendelFacul <161367279+wendellastfacul@users.noreply.github.com>
2024-04-22 21:06:32 -0300

    Add category
    Signed-off-by: wendelFacul <161367279+wendellastfacul@users.noreply.github.com>

---
+++ models.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class BookGuardian(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
+    profile_image = models.ImageField(
+        upload_to="profileImage/%Y/%m/", blank=True, null=True
+    )
+    book_image = models.ImageField(
+        upload_to="bookImage/%Y/%m/", blank=False, null=False
+    )
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    author_book = models.CharField(max_length=100)
+    genero = models.CharField(
+        default="generico",
+        max_length=50,
+        # Status
+        choices=(
+            ("R", "Romance"),
+            ("D", "Drama"),
+            ("T", "Terro"),
+            ("A", "Aventura"),
+            ("AC", "Ação"),
+            ("S", "Suspense"),
+        ), blank=True, null=True)
+    read = models.BooleanField(default=False)
+    created_at = models.DateField(auto_now_add=True)

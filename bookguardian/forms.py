aec8f3b59441a1d565f7eeb1f8aedeaae4e0cabf
wendellast <wendelalves01a@hotmail.com>
2024-04-26 15:53:36 -0300

    add view Login

---
+++ bookguardian/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+class PositionForm(forms.Form):
+    position = forms.CharField()
+
+
--- bookguardian/views.py
+++ bookguardian/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
 from . import models
 
 
@@ -8,3 +9,12 @@ class HomeList(ListView):
     model = models.BookGuardian
     template_name = "index.html"
     context_object_name = "bookguardian"
+
+#User
+class LoginRoute(LoginView):
+    template_name = 'login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('index')

ded2a26eabc420bf79b2e2891db00184e782c1bf
Andra <camileguimaraes463@gmail.com>
2024-04-29 14:56:50 -0300

    filtro funcionando

--- templates/static/assets/js/search.js
+++ templates/static/assets/js/search.js
@@ -1,12 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
+    // Pesquisa
     const searchInput = document.getElementById("search");
     const searchBtn = document.getElementById("searchBtn");
-    const cards = document.querySelectorAll(".card");
+    const msgSearch = document.querySelector("#alertMsg").style;
 
-    function performSearch() {
+    searchBtn.addEventListener("click", function () {
         const searchTerm = searchInput.value.trim().toLowerCase();
+        let itemsFound = false;
 
-        cards.forEach(function (card) {
+        document.querySelectorAll(".card").forEach(function (card) {
             const title = card
                 .querySelector("#title")
                 .textContent.trim()
@@ -17,19 +19,57 @@ document.addEventListener("DOMContentLoaded", function () {
                 .toLowerCase();
             const matchesTitle = title.includes(searchTerm);
             const matchesAuthor = author.includes(searchTerm);
+
             if (matchesTitle || matchesAuthor) {
                 card.style.display = "flex";
+                itemsFound = true;
             } else {
                 card.style.display = "none";
             }
         });
-    }
 
-    searchBtn.addEventListener("click", performSearch);
+        if (!itemsFound) {
+            msgSearch.display = "block";
+        } else {
+            msgSearch.display = "none";
+        }
+    });
 
-    searchInput.addEventListener("keypress", function (event) {
-        if (event.key === "Enter") {
-            performSearch();
+    // Filtro
+    const applyFiltersBtn = document.getElementById("applyFilters");
+    const msgFilter = document.querySelector("#alertMsg").style;
+
+    applyFiltersBtn.addEventListener("click", function () {
+        const genre = document.getElementById("genre").value;
+        const readOrNot = document.getElementById("readOrNot").value;
+        let itemsFound = false;
+
+        document.querySelectorAll(".card").forEach(function (card) {
+            const cardGenre = card.querySelector("#tag").textContent;
+            const cardRead = card
+                .querySelector(".fi")
+                .classList.contains("read");
+            const cardNonRead = card
+                .querySelector(".fi")
+                .classList.contains("nonRead");
+
+            if (
+                (genre === "" || cardGenre === genre) &&
+                (readOrNot === "" ||
+                    (readOrNot === "read" && cardRead) ||
+                    (readOrNot === "nonRead" && cardNonRead))
+            ) {
+                card.style.display = "flex";
+                itemsFound = true;
+            } else {
+                card.style.display = "none";
+            }
+        });
+
+        if (!itemsFound) {
+            msgFilter.display = "block";
+        } else {
+            msgFilter.display = "none";
         }
     });
 });

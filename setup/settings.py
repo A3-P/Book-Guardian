972f552a53c4cb64626bf3236715719aba6ef749
wendellast <wendelalves01a@hotmail.com>
2024-04-30 13:45:22 -0300

    debug --false

--- setup/settings.py
+++ setup/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-nxjvm_=1vo=io18_+vz1ott6uu2qm(=6f#!#aemnwtwwppt*@e"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 

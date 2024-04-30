c8f0a6e7056a11e8219c32a61f92a8c545ace637
wendellast <wendelalves01a@hotmail.com>
2024-04-30 16:27:02 -0300

    add settings config --debug

--- setup/settings.py
+++ setup/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-nxjvm_=1vo=io18_+vz1ott6uu2qm(=6f#!#aemnwtwwppt*@e"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://web-production-2e20.up.railway.app', '127.0.0.1']
 
 
 # Application definition

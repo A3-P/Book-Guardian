c38d8b12357f665e19329debf86632be8d295b92
VilasBoass <marcosjk183@gmail.com>
2024-05-28 14:13:27 -0300

    setup/urls.py

--- setup/urls.py
+++ setup/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
 urlpatterns = [
@@ -8,6 +9,24 @@ urlpatterns = [
     path("", include("bookguardian.urls")),
     path("user/", include("userauths.urls")),
     path('accounts/', include('allauth.urls')),
+     path(
+        "password_reset/", auth_views.PasswordResetView.as_view(), name="password_reset"
+    ),
+    path(
+        "password_reset/done/",
+        auth_views.PasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
